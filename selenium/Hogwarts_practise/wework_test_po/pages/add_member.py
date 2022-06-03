da81720a7fadad06094aa62136d39d8a62824aa0
guonian <guoxiaoli1512@163.com>
2022-06-03 22:22:24 +0800

    add wework_test_po

---
+++ selenium/Hogwarts_practise/wework_test_po/pages/add_member.py
@@ -0,0 +1,3 @@
+#-*- coding:utf-8 -*-
+# @Author : guonian
+# @Time : 2022/06/03 20:21
---
+++ selenium/Hogwarts_practise/wework_test_po/pages/index.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+# @Author : guonian
+# @Time : 2022/06/03 20:08
+from selenium.Hogwarts_practise.wework_test_po.pages.base_page import BasePage
+
+
+class AddMembers(BasePage):
+    def 
