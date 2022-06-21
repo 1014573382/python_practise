035c6e2f2d2f5ae4085b66447087154bc0b61b5a
wangguo@vspn.com <wangguo>
2022-06-21 18:10:28 +0800

    modify element position

--- selenium/base_practise_all/ActionChains/keypress1.py
+++ selenium/base_practise_all/ActionChains/keypress1.py
@@ -2,6 +2,7 @@
  
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
  
@@ -10,12 +11,12 @@ driver.implicitly_wait(10)
 driver.maximize_window()
 driver.get('http://sahitest.com/demo/keypress.htm')
  
-key_up_radio = driver.find_element_by_id('r1') # 监测按键升起
-key_down_radio = driver.find_element_by_id('r2') # 监测按键按下
-key_press_radio = driver.find_element_by_id('r3') # 监测按键按下升起
+key_up_radio = driver.find_element(By.ID, 'r1') # 监测按键升起
+key_down_radio = driver.find_element(By.ID, 'r2') # 监测按键按下
+key_press_radio = driver.find_element(By.ID, 'r3') # 监测按键按下升起
  
-enter = driver.find_elements_by_xpath('//form[@name="f1"]/input')[1] # 输入框
-result = driver.find_elements_by_xpath('//form[@name="f1"]/input')[0] # 监测结果
+enter = driver.find_element(By.XPATH, '//form[@name="f1"]/input')[1] # 输入框
+result = driver.find_element(By.XPATH, '//form[@name="f1"]/input')[0] # 监测结果
  
 # 监测key_down
 key_down_radio.click()
